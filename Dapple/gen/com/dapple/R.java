653fd7f27c7881583c596cdfd0d0dfa7ad192718
lisa <llisa3711@yahoo.com>
2014-03-01 19:41:34 -0500

    added strings xml

--- Dapple/gen/com/dapple/R.java
+++ Dapple/gen/com/dapple/R.java
@@ -24,7 +24,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f08000b;
+        public static final int mileValue=0x7f080002;
+        public static final int muralImageView=0x7f080000;
+        public static final int muralInformation=0x7f080006;
+        public static final int muralTitleHeader=0x7f080001;
+        public static final int pipeView=0x7f080004;
+        public static final int startTour=0x7f080005;
+        public static final int timeValue=0x7f080003;
+        public static final int tourType=0x7f080007;
+        public static final int tourTypeBike=0x7f080008;
+        public static final int tourTypeDrive=0x7f080009;
+        public static final int tourTypeWalk=0x7f08000a;
     }
     public static final class layout {
         public static final int activity_mural_information=0x7f030000;
@@ -37,8 +48,15 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int blankString=0x7f050004;
         public static final int hello_world=0x7f050002;
+        public static final int pipeView=0x7f050005;
+        public static final int startTour=0x7f050006;
         public static final int title_activity_mural_information=0x7f050003;
+        public static final int tourTypeBike=0x7f050007;
+        public static final int tourTypeDrive=0x7f050008;
+        public static final int tourTypeDrive2=0x7f050009;
+        public static final int tourTypeWalk=0x7f05000a;
     }
     public static final class style {
         /** 
