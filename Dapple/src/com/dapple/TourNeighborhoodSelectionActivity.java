923900d35e1decdd1b5e680c4100b054c188a281
Renahan <robert.r.yeager@gmail.com>
2014-03-02 23:45:27 -0500

    Start tour invokes maps with source and dest coordinates

--- Dapple/src/com/dapple/TourNeighborhoodSelectionActivity.java
+++ Dapple/src/com/dapple/TourNeighborhoodSelectionActivity.java
@@ -1,8 +1,11 @@
 package com.dapple;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class TourNeighborhoodSelectionActivity extends Activity {
 
@@ -19,4 +22,18 @@ public class TourNeighborhoodSelectionActivity extends Activity {
 		return true;
 	}
 
+		public void onStartTourClicked(View v)
+		{
+			String current_lat = "39.105633";
+			String current_longi = "-84.484589";
+			String destt_lat = "39.137794";
+			String  dest_longi = "-84.537926";
+			final Intent intent = new Intent(Intent.ACTION_VIEW,
+					 Uri.parse("http://maps.google.com/maps?" + "saddr="+ current_lat+","+current_longi + "&daddr="+ destt_lat+","+ dest_longi));
+
+					 intent.setClassName("com.google.android.apps.maps","com.google.android.maps.MapsActivity");
+					
+					 startActivity(intent); 
+			
+		}
 }
